c9919325b831f3cbb55d551367876b1b037dd907
1chipp <166660222+1chipp@users.noreply.github.com>
2025-02-11 23:02:00 -0800

    Create Profile.js

---
+++ visitingangelsrjs/src/pages/Profile/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+  //test
+  const user = {
+    name: 'Anna',
+    email: 'amuller@example.com',
+    address: '123 12th Ave, Seattle, WA, USA 98122',
+  };
+  
+  return (
+    <div className="profile-container">
+      <h1>Profile</h1>
+      <div className="profile-card">
+        <p><strong>Name:</strong> {user.name}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        <p><strong>Address:</strong> {user.address}</p>
+      </div>
+    </div>
+  );
+};
+  
+export default Profile;
