0530456de0f2e98beb394edc7871b765014cf72f
Anna Muller <annaymuller99@gmail.com>
2025-02-13 10:34:28 -0800

    Adds role and phone number

--- visitingangelsrjs/src/pages/Profile/Profile.js
+++ visitingangelsrjs/src/pages/Profile/Profile.js
@@ -5,6 +5,8 @@ function Profile() {
   //test -- TODO need to get user data from database
   const user = {
     name: 'Anna',
+    role: 'Caregiver',
+    phone: '123-456-7980',
     email: 'amuller@example.com',
     address: '123 12th Ave, Seattle, WA, USA 98122',
   };
@@ -12,13 +14,15 @@ function Profile() {
   return (
     <div className="profile-container">
       <h1>Profile</h1>
-      <div className="profile-card">
-        <p><strong>Name:</strong> {user.name}</p>
-        <p><strong>Email:</strong> {user.email}</p>
-        <p><strong>Address:</strong> {user.address}</p>
-      </div>
+        <div className="profile-card">
+            <p><strong>Name:</strong> {user.name}</p>
+            <p><strong>Role:</strong> {user.role}</p>
+            <p><strong>Phone Number:</strong> {user.phone}</p>
+            <p><strong>Email:</strong> {user.email}</p>
+            <p><strong>Address:</strong> {user.address}</p>
+        </div>
     </div>
   );
 };
-  
+
 export default Profile;
