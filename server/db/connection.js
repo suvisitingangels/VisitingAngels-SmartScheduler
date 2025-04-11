94c23a5f28431527aec5731abc985a70a28508ca
Pham Chi <chi.b.pham001@gmail.com>
2025-04-11 11:14:47 -0700

    Test db connection with server succesfully, wait for log in implementation

--- server/db/connection.js
+++ server/db/connection.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: "suvisitingangelsdb.caji6oq84asv.us-east-1.rds.amazonaws.com",     // e.g., 'localhost'
   user: "VAuser",     // e.g., 'root'
   password: "0aJo0AD#6#6{", // e.g., 'yourpassword'
-  database: "suvisitingangelsdb", // e.g., 'mydatabase'
+  database: "suvisitingangels", // e.g., 'mydatabase'
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
