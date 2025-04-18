7da9ec6ded43bee0407f99c553bb6fd6a9630854
Carlson Sutanto <carlson.sutanto@gmail.com>
2025-04-18 01:00:38 -0700

    Backend modified for deploy Vercel

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// server/app.js
+// server/server.js
 
 /**
      * Server Entry Point
@@ -13,6 +13,8 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
