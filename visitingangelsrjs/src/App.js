c1c0f8ff8103c6e5f27927125490d79cdb8d91b1
Carlson Sutanto <carlson.sutanto@gmail.com>
2025-04-18 01:10:48 -0700

    Code modified for deploy Vercel

--- visitingangelsrjs/src/App.js
+++ visitingangelsrjs/src/App.js
@@ -4,7 +4,7 @@
  * The App.js file is the main entry point for the app's structure and routing setup.
  * It uses React Router to define navigation paths for each page, including Home, Find Caregiver, and Load Data, and includes a Navbar component for easy navigation across all pages.
  * Wrapped in a <Router>, the <Routes> block specifies each path and its corresponding component, while the NotFound component acts as a catch-all for undefined routes, ensuring users see a 404 page for invalid URLs.
- * tThis setup provides a structured and user-friendly navigation flow across the application.
+ * This setup provides a structured and user-friendly navigation flow across the application.
  */
 
 import './App.css';
