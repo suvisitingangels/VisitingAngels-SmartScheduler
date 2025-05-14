e2f4b89712ace3188655fd11d8c5e1866787147f
Anna Muller <annaymuller99@gmail.com>
2025-05-14 13:09:18 -0700

    Adds navigate availability button

--- visitingangelsrjs/src/pages/Caregiver/Home/Home.js
+++ visitingangelsrjs/src/pages/Caregiver/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { jwtDecode } from "jwt-decode";
 import './Home.css';
+import {useNavigate} from "react-router-dom";
 
 function Home() {
     const [availabilityList, setAvailabilityList] = useState([]);
     const [error, setError] = useState('');
     const baseUrl = process.env.REACT_APP_BASE_URL;
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		function to12Hour(time24) {
@@ -59,49 +61,6 @@ function Home() {
 		document.title = "Home | SmartScheduler";
 	}, [baseUrl]);
 
-    // useEffect(() => {
-    //     function to12Hour(time24) {
-    //         const [hStr, m] = time24.split(':');
-    //         let h = parseInt(hStr, 10);
-    //         const suffix = h >= 12 ? 'pm' : 'am';
-    //         h = h % 12 || 12;           // map 0→12, 13→1, 12→12
-    //         return `${h}:${m}${suffix}`;
-	// 	    }
-    //
-    //     const fetchAvailabilities = async () => {
-    //         const token = localStorage.getItem('token');
-    //         if (!token) return setError('Not logged in');
-    //         const { username } = jwtDecode(token);
-    //
-    //         const response = await fetch(`${baseUrl}/api/db/filtered-availabilities/${username}`);
-    //         if (!response.ok) throw new Error(`HTTP Status: ${response.status}`);
-    //         let data = await response.json();
-    //         data = data.availabilities;
-	//
-    //         for (let i = 0; i < data.length; i++) {
-    //             let date = data[i].available_date;
-    //             date = date.split("T")[0];
-    //             data[i].available_date = date;
-	//
-    //             let startTime = data[i].start_time;
-    //             startTime = startTime.slice(0, startTime.length - 3);
-    //             data[i].start_time = to12Hour(startTime);
-	//
-    //             let endTime = data[i].end_time;
-    //             endTime = endTime.slice(0, endTime.length - 3);
-    //             data[i].end_time = to12Hour(endTime);
-    //         }
-    //         setAvailabilityList(data);
-    //         } catch (e) {
-    //             console.error(e);
-    //             setError('Failed to fetch your availabilities');
-    //         }
-    //     };
-	//
-    //     fetchAvailabilities();
-    //     document.title = "Home | SmartScheduler";
-    // }, [baseUrl]);
-
     async function handleDelete(id) {
         try {
             await fetch(`${baseUrl}/api/db/availability/${id}`, { method: 'DELETE' });
@@ -118,11 +77,18 @@ function Home() {
         <div className="availabilites-list">
             <h1 className="title">Availability</h1>
             {availabilityList.length <= 0 ? (
-                <div>No availability</div>
-            ) : (
-                <ul>
-                    {availabilityList.map((availability) => (
-                        <li key={availability.id} className="availability-card">
+				<div className={"no-availability"}>
+					<label>
+						No availability
+					</label>
+					<button onClick={() => navigate('/caregiver/availability')}>
+						Add availability
+					</button>
+				</div>
+			) : (
+				<ul>
+					{availabilityList.map((availability) => (
+						<li key={availability.id} className="availability-card">
                             <span>
                                 <div className="date-info">
                                     <div><b>Date: {availability.available_date}</b></div>
